372dfa9a8dc3a58f746c68f994f555c105da57e2
Padraig Oliver <padraig_oliver@hotmail.com>
2022-02-04 19:51:57 +0000

    readme added

---
+++ README.md
@@ -0,0 +1,5 @@
+# Week 2 Lab - Bash & Git
+Padraig Oliver - X21150451
+[My Github Repo] (https://github.com/padraigoliver/dev_ops)
+---
+To run the script.sh, run "bash script.sh"
--- script.sh
+++ script.sh
@@ -3,6 +3,7 @@
 rm -r fol_1
 rm -r fol_2
 rm .gitignore
+rm README.md
 
 mkdir fol_1
 mkdir fol_2
@@ -20,4 +21,13 @@ touch 2_3.txt
 cd ../
 echo fol_1/ >> .gitignore
 echo fol_2/ >> .gitignore
+
+chmod 600 fol_?/?_[13].txt
+chmod 777 fol_?/?_2.txt
+
+echo '#' Week 2 Lab - Bash '&' Git >> README.md
+echo Padraig Oliver - X21150451 >> README.md
+echo [My Github Repo] '('https://github.com/padraigoliver/dev_ops')' >> README.md
+echo --- >> README.md
+echo To run the script.sh, run '"'bash script.sh'"' >> README.md
 
