73912268555ea04b014878699507ee5555ffbcb4
Padraig Oliver <padraig_oliver@hotmail.com>
2022-02-04 15:27:01 +0000

    first commit

---
+++ script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+mkdir fol_1
+mkdir fol_2
+
+cd fol_1
+touch 1_1.txt
+touch 1_2.txt
+touch 1_3.txt
+
+cd ../fol_2
+touch 2_1.txt
+touch 2_2.txt
+touch 2_3.txt
+
+cd ../
+echo fol_1/ >> .gitignore
+echo fol_2/ >> .gitignore
+
